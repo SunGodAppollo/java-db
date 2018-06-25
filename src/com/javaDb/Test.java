1f174b57d0ebf3f57dd0ce7ebea9e6c32a992cc2
唐磊 <38451390+jhzsyx@users.noreply.github.com>
2018-06-25 18:08:49 +0800

    test出口

--- src/com/javaDb/Test.java
+++ src/com/javaDb/Test.java
@@ -1,20 +1,46 @@
 package com.javaDb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.dbInterface.TableDelete;
+import com.dbInterface.TableInsert;
+import com.dbInterface.TableSelect;
+import com.dbInterface.TableUpdate;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-            DB db=new DB("users");
-            List<Map<String, String>> selectrelt = db.select();
-            DbTool.printRelt(selectrelt);
-            DB db1=new DB("account");
-            List<Map<String, String>> selectrelt1 = db1.select();
-            DbTool.printRelt(selectrelt1);
-            //db.delete(1);
-            
+		
+		
+		/*TableInsert insert = new DBSql("users");
+		Map<String,String> map = new HashMap<>();
+		map.put("id", "1");
+		map.put("user", "tang");
+		map.put("password", "111");
+		insert.insert(map);	*/
+		
+		
+		/*TableSelect select = new DBSql("users");
+		List<Map<String, String>> lists = select.select();
+		System.out.println(lists.toString());*/
+		
+		/*TableUpdate update = new DBSql("users");
+		Map<String,String> whereMap = new HashMap<>();
+		Map<String,String> updateMap = new HashMap<>();
+		updateMap.put("user","qwe");
+		updateMap.put("password","777");
+		whereMap.put("id","1");
+		whereMap.put("qw","2");
+		update.update(updateMap, whereMap);*/
+		
+		TableDelete delete = new DBSql("users") ;
+		Map<String,String> whereMap = new HashMap<>();
+		whereMap.put("id", "2");
+		whereMap.put("user", "tang");
+		delete.delete(whereMap);
 	}
 
 }
